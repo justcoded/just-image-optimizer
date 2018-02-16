d9917f07b114a7418fe895e10f18f7c993e87388
dkonakh <dkonakh@justcoded.co>
2018-02-16 20:01:57 +0200

    Change cUrl function to wp remote, fix jpeg issue

--- services/GooglePagespeed.php
+++ services/GooglePagespeed.php
@@ -7,7 +7,7 @@ use JustCoded\WP\ImageOptimizer\models;
 
 class GooglePagespeed implements ImageOptimizerInterface {
 
-	const API_URL           = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed?';
+	const API_URL = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed?';
 	const OPTIMIZE_CONTENTS = 'https://www.googleapis.com/pagespeedonline/v3beta1/optimizeContents?';
 
 	/**
@@ -35,23 +35,9 @@ class GooglePagespeed implements ImageOptimizerInterface {
 	public function check_api_key() {
 		$check_url = 'http://code.google.com/speed/page-speed/';
 		$url_req   = self::API_URL . 'url=' . $check_url . '&key=' . $this->api_key . '';
-		if ( function_exists( 'file_get_contents' ) ) {
-			$result = @file_get_contents( $url_req );
-		}
-		if ( $result == '' ) {
-			$ch      = curl_init();
-			$timeout = 60;
-			curl_setopt( $ch, CURLOPT_URL, $url_req );
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
-			curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
-			curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-			$result = curl_exec( $ch );
-			curl_close( $ch );
-		}
-		$decode_result = json_decode( $result );
-		if ( isset( $decode_result->responseCode ) && $decode_result->responseCode === 200 ) {
+		$ch        = wp_remote_get( $url_req, array( 'timeout' => 60 ) );
+		$result    = wp_remote_retrieve_response_code( $ch );
+		if ( $result === 200 ) {
 			return true;
 		}
 
@@ -61,9 +47,9 @@ class GooglePagespeed implements ImageOptimizerInterface {
 	/**
 	 * Optimize images and save to destination directory
 	 *
-	 * @param int[]  $attach_ids Attachment ids to optimize.
+	 * @param int[] $attach_ids Attachment ids to optimize.
 	 * @param string $dst Directory to save image to.
-	 * @param models\Log  $log Log object.
+	 * @param models\Log $log Log object.
 	 *
 	 * @return mixed
 	 */
@@ -74,40 +60,42 @@ class GooglePagespeed implements ImageOptimizerInterface {
 		$base_attach_ids = base64_encode( implode( ',', $attach_ids ) );
 		$upload_dir      = WP_CONTENT_DIR;
 		$google_img_path = $dst . '/image/';
-		$wp_filesystem->is_dir($google_img_path) || $wp_filesystem->mkdir($google_img_path);
+		$wp_filesystem->is_dir( $google_img_path ) || $wp_filesystem->mkdir( $google_img_path );
 
 		$images_url = home_url( '/just-image-optimize/' . $base_attach_ids );
 		$log->update_info( 'Optimize request: ' . $images_url );
 
-		$archive_file    = $upload_dir . '/optimize_contents.zip';
-		$file_pointer    = fopen( $archive_file, 'w+' );
-		$source          = self::OPTIMIZE_CONTENTS . 'key=' . $this->api_key . '&url=' . $images_url . '&strategy=desktop';
+		//download archive file with optimized images
+		$archive_file = $upload_dir . '/optimize_contents.zip';
+		$source       = self::OPTIMIZE_CONTENTS . 'key=' . $this->api_key . '&url=' . $images_url . '&strategy=desktop';
+		wp_remote_get( $source,
+			array(
+				'timeout'  => 120,
+				'stream'   => true,
+				'filename' => $archive_file,
+			)
+		);
 
-		$ch              = curl_init();
-		curl_setopt( $ch, CURLOPT_URL, $source );
-		curl_setopt( $ch, CURLOPT_FILE, $file_pointer );
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-		curl_exec( $ch );
-		curl_close( $ch );
-		fclose( $file_pointer );
-
-		$log->update_info( 'Downloaded: ' . $archive_file . ', ' . ((int)@filesize($archive_file)) . 'B' );
+		$log->update_info( 'Downloaded: ' . $archive_file . ', ' . ( (int) @filesize( $archive_file ) ) . 'B' );
 
 		// optimized images are placed under /image folder inside the archive, so $google_img_path = $dst . '/image'.
 		$unzipfile = unzip_file( $archive_file, $dst );
 		if ( ! is_wp_error( $unzipfile ) ) {
 			// Get array of all source files.
-			$files = scandir( $google_img_path );
+			$files   = scandir( $google_img_path );
 			$counter = 0;
 			foreach ( $files as $file ) {
 				if ( in_array( $file, array( '.', '..' ), true ) ) {
 					continue;
 				}
 				copy( $google_img_path . $file, $dst . $file );
-				$counter++;
+				if ( strpos( $file, '.jpg' ) !== false ) {
+					copy( $google_img_path . $file, $dst . str_replace( '.jpg', '.jpeg', $file ) );
+				}
+				$counter ++;
 			}
 			if ( is_dir( $google_img_path ) ) {
-				$wp_filesystem->rmdir( $google_img_path );
+				$wp_filesystem->rmdir( $google_img_path, true );
 			}
 			unlink( $archive_file );
 
