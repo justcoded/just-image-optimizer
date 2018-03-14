75c8ac9110f18b640bce30cb4c1be597bb8049a5
alex_prokopenko <ab.prokopenko@gmail.com>
2018-03-14 14:10:21 +0200

    Updated column try again styles

--- views/media/column.php
+++ views/media/column.php
@@ -15,15 +15,16 @@ $total_stats  = $model->get_total_attachment_stats( $id );
 	<?php if ( ! empty( $total_stats[0]->percent ) ) : ?>
 		<p><?php echo esc_html( $total_stats[0]->percent ); ?>% saved
 			(<?php echo esc_html( ! empty( $total_stats[0]->saving_size ) ? size_format( $total_stats[0]->saving_size ) : 0 ); ?>)
-			<?php if ( Media::STATUS_PARTIALY_PROCESSED === $image_status ) : ?>
-				<br>
-				<a class="optimize-now" href="#<?php echo esc_attr( $id ); ?>" data-attach-id="<?php echo esc_attr( $id ); ?>">
-					try again
-				</a>
-			<?php endif; ?>
 		</p>
 		<p>disk usage: <?php echo size_format( $total_stats[0]->disk_usage ); ?>
 			(<?php echo esc_html( $model->get_count_images( $id ) ); ?> images) </p>
+		<?php if ( Media::STATUS_PARTIALY_PROCESSED === $image_status ) : ?>
+			<em>&nbsp; * can be better,
+			<a class="optimize-now" href="#<?php echo esc_attr( $id ); ?>" data-attach-id="<?php echo esc_attr( $id ); ?>">
+				try again
+			</a>
+			</em>
+		<?php endif; ?>
 	<?php endif; ?>
 <?php elseif ( Media::STATUS_IN_QUEUE === $image_status ) : ?>
 	<p>Queued (#<?php echo esc_html( $id ); ?>)</p>
